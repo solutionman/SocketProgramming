cc5075c2ed9203179498e3e82ea3f3687663109d
lom <lom@ssvet.info>
2020-06-24 09:24:45 +0700

    codeStyle

--- client.c
+++ client.c
@@ -20,8 +20,7 @@ int main(int argc, char *argv[]) {
     }
 
     memset(recvBuff, '0',sizeof(recvBuff));
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         printf("\n Error : Could not create socket \n");
         return 1;
     }
@@ -31,29 +30,24 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000);
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
-    {
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0){
         printf("\n inet_pton error occured\n");
         return 1;
     }
 
-    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if( connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
        printf("\n Error : Connect Failed \n");
        return 1;
     }
 
-    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-    {
+    while ( (n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0){
         recvBuff[n] = 0;
-        if(fputs(recvBuff, stdout) == EOF)
-        {
+        if(fputs(recvBuff, stdout) == EOF){
             printf("\n Error : Fputs error\n");
         }
     }
 
-    if(n < 0)
-    {
+    if(n < 0){
         printf("\n Read error \n");
     }
 
